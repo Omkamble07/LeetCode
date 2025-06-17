4ac6d658454855982914d3e42dd4caded15aa2f5
Om Kamble <168065317+Omkamble07@users.noreply.github.com>
2025-06-17 22:05:41 +0530

    Time: 0 ms (100%), Space: 8.6 MB (43.02%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long reverse = 0;
+        int digit, originalNum = x;
+        if(x<0) return false;
+        while(x>0){
+            digit = x % 10;
+            reverse = reverse * 10 + digit;
+            x = x/10;
+        }
+        if(originalNum == reverse){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
