8d560d987cf7bcc2accb6e2f4636c4d7dbd85ad8
Om Kamble <168065317+Omkamble07@users.noreply.github.com>
2026-03-03 13:33:41 +0530

    Time: 7 ms (84.52%), Space: 64.6 MB (69.82%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals) {
+    if(intervals.length <= 1 ) return intervals;
+
+    intervals.sort((a,b)=> a[0] - b[0]);
+
+    let result = [];
+
+    result.push(intervals[0]);
+
+    for(let i=0; i<intervals.length; i++){
+        let prev = result[result.length-1] ;
+        let curr = intervals[i] ;
+
+        if(curr[0] <= prev[1]){
+            prev[1] = Math.max(prev[1], curr[1]);
+        }else{
+            result.push(curr);
+        }  
+    }
+
+    return result;
+};
