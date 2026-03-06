e08a8c523d001143e6e26c0ae3823d28ca76bb86
Om Kamble <168065317+Omkamble07@users.noreply.github.com>
2026-03-06 10:03:30 +0530

    Time: 0 ms (100%), Space: 54.1 MB (74.36%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function(matrix) {
+    let n = matrix.length ;
+
+    // transpose
+    for(let i=0; i<n; i++){
+        for(let j=i; j<n; j++){
+            
+            let temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i] 
+            matrix[j][i] = temp
+        }
+    }
+
+    // reverse each row
+    for(let i=0; i<n; i++){
+        matrix[i].reverse()
+    }
+};
